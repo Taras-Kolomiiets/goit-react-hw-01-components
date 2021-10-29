ea1fb01fd05d931ab34501a4534ac01a0a51a782
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-10-29 21:34:04 +0300

    fix friends list

--- src/components/Friends/FriendsList.jsx
+++ src/components/Friends/FriendsList.jsx
@@ -1,18 +1,21 @@
-import FriendsListItem from "./FriendsListItem/FriendsListItem";
-import s from "./FriendsList.module.css";
-import PropTypes from "prop-types";
+import FriendsListItem from './FriendsListItem/FriendsListItem';
+import s from './FriendsList.module.css';
+import PropTypes from 'prop-types';
 
 function FriendsList({ friends }) {
-  const friendsList = friends.map((friend) => (
-    <FriendsListItem
-      key={friend.id}
-      avatar={friend.avatar}
-      id={friend.id}
-      name={friend.name}
-      isOnline={friend.isOnline}
-    />
-  ));
-  return <ul className={s.friendList}>{friendsList}</ul>;
+  return (
+    <ul className={s.friendList}>
+      {friends.map(friend => (
+        <FriendsListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          id={friend.id}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
 }
 
 FriendsList.propTypes = {
